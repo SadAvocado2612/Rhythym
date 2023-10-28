8b4d9a495a3583bc3e08bbe899d409eb7fa43f78
DarthSid12 <65533662+DarthSid12@users.noreply.github.com>
2023-10-28 21:46:51 +0530

    Authentication system working

--- test.py
+++ test.py
@@ -1,2 +1,31 @@
-import os
-os.add_dll_directory(os.getcwd())
+import itertools
+
+costs = [[18,3,18],[14,14,4],[15,3,17]]
+
+def min_cost_vertex_coloring_enumeration(costs):
+    dpTable = [costs[0]]
+    l=[[0],[1],[2]]
+    for i in costs[1:]:
+        k= []
+        for j in range(len(i)):
+            smallest = 10000
+            smallestIndex = 0
+            for a in range(3):
+                if dpTable[-1][a]<smallest and a!=j:
+                    smallest = dpTable[-1][a]
+                    smallestIndex = a
+            i[j] +=smallest
+            k.append(l[smallestIndex]+[j])
+        l=k   
+        dpTable.append(i)
+    print(dpTable)
+    print(min(dpTable[-1]))
+    print(l[dpTable[-1].index(min(dpTable[-1]))])
+    
+            
+        
+# def opt(k,i):
+    
+    
+min_cost_vertex_coloring_enumeration(costs)
+
